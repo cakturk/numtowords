2f6ce91ef2faeb09e2933433c1599379be42f33e
Cihangir Akturk <cakturk@gmail.com>
2012-08-21 19:07:28 +0300

    dummy: I don't know what I have done in this commit.
    Signed-off-by: Cihangir Akturk <cakturk@gmail.com>

--- numtowords.c
+++ numtowords.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
+#include <string.h>
+#include <errno.h>
 
 #define ARRAY_SIZE(__arr) (sizeof(__arr) / sizeof((__arr)[0]))
+
+struct buffer_struct {
+    size_t size;
+    char *buffer;
+    char *rev_ptr;
+} /* optional variable list */;
+
+static char *out_buf;
+static char *out_ptr;
+static size_t out_buf_size;
 
 static char *numbers[] = {
     "bir", "iki", "uc", "dort",
@@ -11,11 +23,37 @@ static char *numbers[] = {
     "milyon", "milyar", "trilyon"
 };
 
-static const char
-*numstr(int digit, int place_val, int triple)
+static int prepend_str(const char *str)
+{
+    size_t len = strlen(str);
+    if (out_ptr - len < out_buf)
+        return -ERANGE;
+
+    printf("buf = %u, ptr = %u, len = %u\n",
+            (unsigned long)out_buf,
+            (unsigned long)out_ptr, len);
+
+    out_ptr -= len;
+    memcpy(out_ptr, str, len);
+
+    return 0;
+}
+
+static inline int prepend_char(char ch)
+{
+    if (out_ptr - 1 < out_buf)
+        return -ERANGE;
+
+    --out_ptr;
+    *out_ptr = ch;
+
+    return 0;
+}
+
+static const char *numstr(int digit, int place_val)
 {
     char *ret = NULL;
-    int num = digit * place_val;
+    long long num = digit * place_val;
 
     if (num < 11 && num > 0) {
         ret = numbers[num - 1];
@@ -53,6 +91,15 @@ static const char
     case 1000:
         ret = numbers[19];
         break;
+    case 1000000:
+        ret = numbers[20];
+        break;
+    case 1000000000:
+        ret = numbers[21];
+        break;
+    case 1000000000000:
+        ret = numbers[22];
+        break;
     }
 
     return ret;
@@ -60,6 +107,52 @@ static const char
 
 static int __num_to_words(char *buf, long num);
 static inline int _pow(int x, int y);
+
+static const char *__numstr(int digit, int pos)
+{
+    const char *ret = NULL;
+    int triple = pos / 3;
+    int place_val = _pow(10, pos % 3);
+
+    switch (pos % 3) {
+    case 0:
+        if (triple) {
+            ret = numstr(_pow(10, triple * 3), 1);
+            if (prepend_str(ret) != 0)
+                goto err_out;
+            printf("new = %s\n", ret);
+            if (digit > 1) {
+                ret = numstr(digit, 1);
+                if (prepend_str(ret) != 0)
+                    goto err_out;
+                printf("new = %s\n", numstr(digit, 1));
+            }
+        } else {
+            ret = numstr(digit ,1);
+            if (prepend_str(ret) != 0)
+                goto err_out;
+            printf("new = %s\n", ret);
+        }
+        break;
+    case 1:
+        ret = numstr(digit * place_val, 1);
+        printf("new = %s\n", ret);
+        if (prepend_str(ret) != 0)
+            goto err_out;
+        break;
+    case 2:
+        ret = numstr(place_val, 1);
+        if (prepend_str(ret) != 0)
+            goto err_out;
+        printf("new = %s\n", ret);
+        ret = numstr(digit, 1);
+        printf("new = %s\n", ret);
+        break;
+    }
+
+err_out:
+    return ret;
+}
 
 int num_to_words(char *buf, double num)
 {
@@ -79,22 +172,30 @@ static int __num_to_words(char *buf, long num)
     (void)buf;
     unsigned char rmost_digit;
     int pos = 0;
-    int place_value;
     int triple = 0;
+
+    char dummy_buf[256];
+    out_buf = dummy_buf;
+    out_buf_size = ARRAY_SIZE(dummy_buf);
+    out_ptr = out_buf + out_buf_size - 1;
+    *out_ptr = '\0';
 
     /* Extract rightmost digit and shift right */
     do {
         rmost_digit = num % 10;
-        place_value = _pow(10, pos % 3);
+        __numstr(rmost_digit, pos);
+
         if (pos % 3 == 0)
             ++triple;
         ++pos;
 
         /* TODO: numstr(digit, place_value, triple) */
-        printf("rightmost = %d, val = %s, place_val = %d, triple = %d\n",
-                rmost_digit, numstr(rmost_digit ,place_value, triple),
-                place_value, triple);
+       // printf("rightmost = %d, val = %s, place_val = %d, triple = %d\n",
+       //         rmost_digit, numstr(rmost_digit ,place_value),
+       //         place_value, triple);
     } while (num /= 10);
+
+    printf("buf contains the string = %s\n", out_ptr);
 
     return 0;
 }
